f78f8ce8a160b8118b26f9bf55c16f85cf187991
Cristiano Assis <cristiano.aa@gmail.com>
2021-08-10 22:24:55 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/financeira/dh/Emprestimo1Application.java
@@ -0,0 +1,13 @@
+package br.com.financeira.dh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emprestimo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Emprestimo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/financeira/dh/Emprestimo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financeira.dh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emprestimo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
